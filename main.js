b640833bf41e054fe3b8d105b39583fb5ffa2ef1
Alisson Rolim <167247292+Alisson-Rolim-knies@users.noreply.github.com>
2024-10-30 18:48:25 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar'); 
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
+
+
+    fetch('https://api.github.com/users/Alisson-Rolim-Knies')
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(json){
+            nameElement.innerText = json.name;
+            usernameElement.innerText = json.login;
+            avatarElement.src = json.avatar_url; 
+            reposElement.innerText = json.public_repos; 
+            followersElement.innerText = json.followers;
+            followingElement.innerText = json.following; 
+            linkElement.href = json.html_url; 
+        })
+        .catch(function(error){
+            console.error('Houve um problema com a operação de busca:', error); 
+        });
+});
+
+
