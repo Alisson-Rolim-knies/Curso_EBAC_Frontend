84638790769318f315599223743efcdd7965b7bb
Alisson Rolim <167247292+Alisson-Rolim-knies@users.noreply.github.com>
2025-02-26 16:21:03 -0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicar(5, 4));
+console.log(saudacao("Alisson_r_k"));
